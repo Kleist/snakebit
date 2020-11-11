ce0aafa5b4e8803c5a47ffa421256c291a1830ca
Andreas Helbech Kleist <aks@cryptera.com>
2020-11-11 23:31:58 +0100

    Control heart fill with buttons

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,25 @@ use heapless::Vec;
 
 use microbit::hal::nrf51;
 
-// use microbit::display::image::GreyscaleImage;
-use microbit::display::{self, Display, /*Frame,*/ MicrobitDisplayTimer, MicrobitFrame};
+use microbit::display::image::GreyscaleImage;
+use microbit::display::{self, Display, Frame, MicrobitDisplayTimer, MicrobitFrame};
 
 use rtic::app;
 
-use snakebit::GameState;
-use snakebit::Direction;
 use snakebit::Coord;
+use snakebit::Direction;
+use snakebit::GameState;
+
+fn heart_image(inner_brightness: u8) -> GreyscaleImage {
+    let b = inner_brightness;
+    GreyscaleImage::new(&[
+        [0, 7, 0, 7, 0],
+        [7, b, 7, b, 7],
+        [7, b, b, b, 7],
+        [0, 7, b, 7, 0],
+        [0, 0, 7, 0, 0],
+    ])
+}
 
 #[app(device = microbit::hal::nrf51, peripherals = true)]
 const APP: () = {
@@ -67,8 +78,13 @@ const APP: () = {
         p.GPIOTE.intenset.write(|w| w.in1().set_bit());
         p.GPIOTE.events_in[1].write(|w| unsafe { w.bits(0) });
 
-        let mut state = GameState{height: 5, width: 5, snake: Vec::new(), dir: Direction::North};
-        let _ = state.snake.push(Coord{x:2,y:0});
+        let mut state = GameState {
+            height: 5,
+            width: 5,
+            snake: Vec::new(),
+            dir: Direction::North,
+        };
+        let _ = state.snake.push(Coord { x: 2, y: 0 });
 
         let mut timer = MicrobitDisplayTimer::new(p.TIMER1);
         display::initialise_display(&mut timer, &mut p.GPIO);
@@ -82,8 +98,10 @@ const APP: () = {
         }
     }
 
-    #[task(binds = GPIOTE, priority = 1, resources=[gpiote])]
-    fn btn(cx: btn::Context) {
+    #[task(binds = GPIOTE, priority = 1, resources=[gpiote, display])]
+    fn btn(mut cx: btn::Context) {
+        static mut FRAME: MicrobitFrame = MicrobitFrame::const_default();
+        static mut STEP: u8 = 3;
         let gpiote = cx.resources.gpiote;
         let a_pressed = gpiote.events_in[0].read().bits() != 0;
         let b_pressed = gpiote.events_in[1].read().bits() != 0;
@@ -101,5 +119,28 @@ const APP: () = {
         /* Clear events */
         gpiote.events_in[0].write(|w| unsafe { w.bits(0) });
         gpiote.events_in[1].write(|w| unsafe { w.bits(0) });
+
+        let increment:u8 = match(*STEP, a_pressed, b_pressed) {
+            (7, true, false) => 7,
+            (0, false, true) => 0,
+            (x, true, _) => x+1,
+            (x, _, true) => x-1,
+            (x, _, _) => x,
+        };
+        defmt::info!("increment: {:?}", increment);
+        *STEP = increment;
+        FRAME.set(&mut heart_image(*STEP));
+        cx.resources.display.lock(|display| {
+            display.set_frame(&FRAME);
+        });
+    }
+
+    #[task(binds = TIMER1, priority = 2, resources = [display_timer, gpio, display])]
+    fn timer1(mut cx: timer1::Context) {
+        display::handle_display_event(
+            &mut cx.resources.display,
+            cx.resources.display_timer,
+            cx.resources.gpio,
+        );
     }
 };

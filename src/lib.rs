628f08c9ae471baad3183a10bb663aeae12e9075
Andreas Helbech Kleist <aks@cryptera.com>
2020-11-11 23:12:49 +0100

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -54,15 +54,6 @@ pub fn step(state: &mut GameState) {
     state.snake[0] = next(&state.snake[0], &state.dir).unwrap()
 }
 
-#[defmt::timestamp]
-fn timestamp() -> u64 {
-    static COUNT: AtomicUsize = AtomicUsize::new(0);
-    // NOTE(no-CAS) `timestamps` runs with interrupts disabled
-    let n = COUNT.load(Ordering::Relaxed);
-    COUNT.store(n + 1, Ordering::Relaxed);
-    n as u64
-}
-
 /// Terminates the application and makes `probe-run` exit with exit-code = 0
 pub fn exit() -> ! {
     loop {
